58c77159edd40b4a949b343873a98a1f2b4608d5
Bing Gong <gongbing1112@gmail.com>
2026-08-13 19:58:07 +0000

    add nvidia_brev_infer_finetune_decoder.sh

---
+++ v2.0/HPC_scripts/nvidia_brev_infer_finetune_decoder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+source /home/nvidia/miniforge3/etc/profile.d/conda.sh
+conda activate /home/nvidia/miniforge3/envs/s2s_env
+
+echo nvidia-smi
+
+# MPI and OpenMP settings
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+export NUM_TASKS_PER_NODE=4
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+echo "NUM_TASKS_PER_NODE= ${NUM_TASKS_PER_NODE}"
+
+config_file=../config/exp16_nvidia_v3.yaml
+#finetune_ckpt=/data/bing/S2S/v2.0/HPC_scripts/results/S2S/finetune_finetune_decoder_crps/training_checkpoints/finetune_ckpt_3200.tar
+#finetune_ckpt=/data/bing/S2S/v2.0/HPC_scripts/results/S2S/rollout_finetune_finetune_rollout_crps/training_checkpoints/rollout_ckpt_1000.tar
+finetune_ckpt=/data/bing/S2S/v2.0/HPC_scripts/results/S2S/rollout_finetune_finetune_rollout_crps_multipsteps/training_checkpoints/rollout_ckpt_1400.tar
+# inference with CRPS fine-tuned decoder
+python -m torch.distributed.launch --master_port=29503 --nproc_per_node=$NUM_TASKS_PER_NODE ../inference.py \
+    --yaml_config=$config_file \
+    --run_num=finetune_rollout_crps_v2  \
+    --finetune_ckpt=$finetune_ckpt
